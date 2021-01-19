8b9cd625725f95006d99d52bd067256c1cb76d59
Jacobstephens17 <64627086+Jacobstephens17@users.noreply.github.com>
2021-01-19 12:37:56 -0800

    wrapped app in provider and created store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+
+const store = createStore(App, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, rootElement);
